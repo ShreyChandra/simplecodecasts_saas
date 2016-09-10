083596f7201da564f0397eaaafdd28b4241f41f0
Shrey Chandra <shrey.2012.chandra@gmail.com>
2016-09-10 10:09:36 +0000

    Added mailer and view

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -9,8 +9,15 @@ class ContactsController < ApplicationController
         
         
         if @contact.save
+            
+            name = params[:contact][:name]
+            email = params[:contact][:email]
+            body = params[:contact][:comments]
+            
+            ContactMailer.contact_email(name , email , body).deliver
             flash[:success] = "Message Sent."
             redirect_to new_contact_path
+            
         else
             flash[:danger] = "Error."
             redirect_to new_contact_path
