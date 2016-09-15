865f7c862cdd99b34919877a961ba72d82d6b9de
Shrey Chandra <shrey.2012.chandra@gmail.com>
2016-09-15 19:28:52 +0000

    styel and general improvements

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,15 @@
 class UsersController < ApplicationController
     before_action :authenticate_user!
-    
+
     def new
         @user = User.new(params[:user])
+        
+    end
+    
+    
+   
+    def index
+        @users = User.all
     end
    
     def show
