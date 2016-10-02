86acb9658083f5a52ad9037032a807c1757f4897
Shrey Chandra <shrey.2012.chandra@gmail.com>
2016-10-02 18:27:13 +0000

    using rescue

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
     def home
         @basic_plan = Plan.find(1)
-        rescue ActiveRecord::RecordNotFound 
+        rescue ActiveRecord::RecordNotFound => e
         @basic_plan = nil
         @pro_plan = Plan.find(2)
     end
