1f712c172c26bb97a0f21a9840b292e4647e4544
Shrey Chandra <shrey.2012.chandra@gmail.com>
2016-10-04 05:19:16 +0000

    seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Plan.create(id: 1,name: 'basic',price:'0.0', created_at: "2016-09-13 16:20:05", updated_at: "2016-09-13 16:20:05")
+Plan.create(id: 2,name: 'pro',price:'10.0', created_at: "2016-09-13 16:20:57", updated_at: "2016-09-13 16:20:57")
